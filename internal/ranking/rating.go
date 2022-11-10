d8a8e7bd3bcffc08984cf63b9fdf44ef54bf7c77
Robby Barton <rbartonsc@gmail.com>
2022-11-10 00:35:05 -0500

    write a proper SRS and rename previous to SOE

--- internal/ranking/rating.go
+++ internal/ranking/rating.go
@@ -3,6 +3,7 @@ package ranking
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"sort"
 
 	"github.com/robby-barton/stats-api/internal/database"
@@ -11,7 +12,7 @@ import (
 	"gonum.org/v1/gonum/stat"
 )
 
-type gameSpread struct {
+type gameSpreadSOE struct {
 	team     int64
 	spread   float64
 	opponent int64
@@ -40,7 +41,7 @@ func PrintSRS(teamList TeamList, top int) {
 	}
 }
 
-func (r *Ranker) srs(teamList TeamList) error {
+func (r *Ranker) soe(teamList TeamList) error {
 	// range order over a map is not deterministic, so create a slice to ensure
 	// order when creating vectors/matrices for SoE
 	var teamOrder []int64
@@ -142,7 +143,7 @@ func (r *Ranker) srs(teamList TeamList) error {
 		gameStatsMap[stat.GameId] = append(gameStatsMap[stat.GameId], stat)
 	}
 
-	teamGameInfo := map[int64][]*gameSpread{}
+	teamGameInfo := map[int64][]*gameSpreadSOE{}
 	var spreads []float64
 	for id, stats := range gameStatsMap {
 		if len(stats) != 2 {
@@ -151,12 +152,12 @@ func (r *Ranker) srs(teamList TeamList) error {
 
 		spread := stats[0].Score - stats[1].Score
 		spreads = append(spreads, float64(spread), float64(-spread))
-		teamGameInfo[stats[0].TeamId] = append(teamGameInfo[stats[0].TeamId], &gameSpread{
+		teamGameInfo[stats[0].TeamId] = append(teamGameInfo[stats[0].TeamId], &gameSpreadSOE{
 			team:     stats[0].TeamId,
 			spread:   float64(spread),
 			opponent: stats[1].TeamId,
 		})
-		teamGameInfo[stats[1].TeamId] = append(teamGameInfo[stats[1].TeamId], &gameSpread{
+		teamGameInfo[stats[1].TeamId] = append(teamGameInfo[stats[1].TeamId], &gameSpreadSOE{
 			team:     stats[1].TeamId,
 			spread:   float64(-spread),
 			opponent: stats[0].TeamId,
@@ -225,6 +226,163 @@ func (r *Ranker) srs(teamList TeamList) error {
 	var prev float64
 	var prevRank int64
 	for rank, id := range teamOrder {
+		team := teamList[id]
+
+		if team.SRS == prev {
+			team.SRSRank = prevRank
+		} else {
+			team.SRSRank = int64(rank + 1)
+			prev = float64(team.SRS)
+			prevRank = team.SRSRank
+		}
+
+		if max-min > 0 {
+			team.SRSNorm = (team.SRS - min) / (max - min)
+		}
+	}
+
+	return nil
+}
+
+type gameSpreadSRS struct {
+	team     int64
+	spread   int64
+	opponent int64
+}
+
+func (r *Ranker) srs(teamList TeamList) error {
+	runs := 10000
+	requiredGames := 6
+	// get previous season games just to be ready
+	var prevSeason []database.Game
+	if err := r.DB.Where("season = ?", year-1).
+		Order("start_time desc").Find(&prevSeason).Error; err != nil {
+
+		return err
+	}
+
+	teamGames := make(map[int64][]int64)
+	var games []int64
+	found := make(map[int64]bool)
+	for id, team := range teamList {
+		divGames := 0
+		for _, g := range team.Schedule {
+			divGames++
+			teamGames[id] = append(teamGames[id], g.GameId)
+			if _, ok := found[g.GameId]; !ok {
+				games = append(games, g.GameId)
+				found[g.GameId] = true
+			}
+		}
+		if divGames < requiredGames {
+			for _, game := range prevSeason {
+				if game.HomeId == id || game.AwayId == id {
+				divGames++
+				teamGames[id] = append(teamGames[id], game.GameId)
+				if _, ok := found[game.GameId]; !ok {
+					games = append(games, game.GameId)
+					found[game.GameId] = true
+				}
+				if divGames >= requiredGames {
+					break
+				}
+			}
+			}
+		}
+	}
+
+	var gameStats []database.TeamGameStats
+	gameStatsMap := make(map[int64][]database.TeamGameStats)
+	if err := r.DB.Where("game_id in (?)", games).Find(&gameStats).Error; err != nil {
+		return err
+	}
+	for _, stat := range gameStats {
+		gameStatsMap[stat.GameId] = append(gameStatsMap[stat.GameId], stat)
+	}
+
+	teamGameInfo := map[int64][]*gameSpreadSRS{}
+	for id, stats := range gameStatsMap {
+		if len(stats) != 2 {
+			return errors.New(fmt.Sprintf("Not correct number of game stats (%d)", id))
+		}
+
+		spread := stats[0].Score - stats[1].Score
+		if spread > 24 {
+			spread = 24
+		} else if spread < 7 && spread > 0 {
+			spread = 7
+		} else if spread > -7 && spread < 0 {
+			spread = -7
+		} else if spread < -24 {
+			spread = -24
+		}
+
+		firstId := stats[0].TeamId
+		if _, ok := teamList[firstId]; !ok {
+			firstId = 0
+		}
+		secondId := stats[1].TeamId
+		if _, ok := teamList[secondId]; !ok {
+			secondId = 0
+		}
+		teamGameInfo[firstId] = append(teamGameInfo[firstId], &gameSpreadSRS{
+			team:     firstId,
+			spread:   spread,
+			opponent: secondId,
+		})
+		teamGameInfo[secondId] = append(teamGameInfo[secondId], &gameSpreadSRS{
+			team:     secondId,
+			spread:   -spread,
+			opponent: firstId,
+		})
+	}
+
+	ratings := map[int64]float64{}
+	for id, spreads := range teamGameInfo {
+		var avg int64
+		for _, spread := range spreads {
+			avg += spread.spread
+		}
+		ratings[id] = float64(avg) / float64(len(spreads))
+	}
+
+	adjRatings := ratings
+	for i := 0; i < runs; i++ { // guard against oscillating by capping runs
+		nextRating := map[int64]float64{}
+		for id, games := range teamGameInfo {
+			var oppAvg float64
+			for _, game := range games {
+				oppAvg += adjRatings[game.opponent]
+			}
+			oppAvg /= float64(len(games))
+			nextRating[id] = ratings[id] + oppAvg
+		}
+
+		// when they stop changing, we've peaked
+		if reflect.DeepEqual(adjRatings, nextRating) {
+			break
+		}
+		adjRatings = nextRating
+	}
+	delete(adjRatings, 0)
+
+	for id, rating := range adjRatings {
+		teamList[id].SRS = rating
+	}
+
+	var ids []int64
+	for id := range teamList {
+		ids = append(ids, id)
+	}
+	sort.Slice(ids, func(i, j int) bool {
+		return teamList[ids[i]].SRS > teamList[ids[j]].SRS
+	})
+
+	max := teamList[ids[0]].SRS
+	min := teamList[ids[len(ids)-1]].SRS
+	var prev float64
+	var prevRank int64
+	for rank, id := range ids {
 		team := teamList[id]
 
 		if team.SRS == prev {
